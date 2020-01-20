1a65ba144b1b105873a6fd2f4bfcf3639224ff9f
Aleksandr Shchilkin <crazyredkitten@icloud.com>
2020-01-20 05:34:28 +0200

    Added placeholder for Plans Section

--- src/components/pages/home/Plans.js
+++ src/components/pages/home/Plans.js
@@ -5,6 +5,19 @@ const Plans = () => {
     <div className='container-fluid text-white pt-5 pb-5 bg-secondary'>
       <div className='container'>
         <h1 class='display-5 pt-3 pb-3'>Plans</h1>
+        <p>
+          <span className='badge badge-danger'>TODO </span>{" "}
+          <span className='badge badge-info'>Add description</span> Lorem ipsum
+          dolor sit amet, consectetur adipiscing elit. Nulla faucibus vitae
+          risus at fringilla. Vivamus ut placerat elit. Fusce dignissim urna
+          eget interdum ultricies. Nulla nec ultricies orci, id mollis erat.
+          Aenean augue leo, congue nec ante at, posuere molestie neque. Donec in
+          ipsum pretium, placerat ipsum et, malesuada purus. Sed cursus, ligula
+          eu ullamcorper pharetra, mi felis vehicula ante, ut cursus mi purus id
+          nulla. Sed sit amet lorem mauris. Integer a auctor ipsum. Praesent in
+          leo feugiat, luctus neque nec, consectetur odio. Morbi vel vestibulum
+          arcu.
+        </p>
       </div>
     </div>
   );
