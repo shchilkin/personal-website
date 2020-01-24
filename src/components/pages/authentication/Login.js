366397cfe80b3562dce485f99a557b35cb190301
Aleksandr Shchilkin <crazyredkitten@icloud.com>
2020-01-24 13:44:25 +0200

    Added Login and Registration fields

--- src/components/pages/authentication/Login.js
+++ src/components/pages/authentication/Login.js
@@ -1,9 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
+import { Eye } from "react-bootstrap-icons";
 
 const Login = () => {
+  const [email, setEmail] = useState([{ email: "" }]);
+  const [password, setPassword] = useState([{ password: "" }]);
+  console.log(email, password);
+  const [data, setData] = useState([email, password]);
+
+  const changeEmail = event => {
+    setEmail({ email: event.target.value });
+  };
+  const changePassword = event => {
+    setPassword({ password: event.target.value });
+  };
+
+  const click = event => {
+    event.preventDefault();
+    setData(email, password);
+    console.log(email, password, data);
+  };
+
   return (
-    <div>
-      <h1>Login</h1>
+    <div className='container text-center'>
+      <div className='alert alert-warning mt-2'>
+        Login page is in development
+      </div>
+      <h1 className='mt-3 mb-3'>
+        <span style={{ color: "cornflowerblue" }}>L</span>ogin
+      </h1>
+      <div className='d-flex justify-content-center'>
+        <form className='mb-3 col-lg-5'>
+          <div className='form-group'>
+            <label htmlFor='InputEmail1'>Email address</label>
+            <input
+              type='email'
+              className='form-control'
+              id='exampleInputEmail1'
+              aria-describedby='emailHelp'
+              placeholder='Enter email'
+              onChange={changeEmail}
+            />
+          </div>
+          <div className='form-group'>
+            <label htmlFor='InputPassword'>Password</label>
+            <div className='input-group'>
+              <input
+                type='password'
+                className='form-control'
+                id='exampleInputPassword'
+                placeholder='Password'
+                onChange={changePassword}
+              />
+              <div class='input-group-append'>
+                <button
+                  className='btn btn-outline-secondary'
+                  type='button'
+                  id='button-addon2'
+                  data-toggle='button'
+                  aria-pressed='false'
+                  autocomplete='off'
+                >
+                  <Eye />
+                </button>
+              </div>
+            </div>
+          </div>
+
+          <button onClick={click} type='submit' className='btn btn-primary'>
+            Log in
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
--- src/components/pages/authentication/Registration.js
+++ src/components/pages/authentication/Registration.js
@@ -2,8 +2,70 @@ import React from "react";
 
 const Registration = () => {
   return (
-    <div>
-      <h1>Registration</h1>
+    <div className='container text-center'>
+      <div className='alert alert-warning mt-2'>
+        Registration page is in development
+      </div>
+      <h1 className='mt-3 mb-3'>
+        <span style={{ color: "cornflowerblue" }}>R</span>egistration
+      </h1>
+      <div className='d-flex justify-content-center'>
+        <form className='mb-3 col-lg-5'>
+          <div className='form-group'>
+            <label htmlFor='InputFirstName'>First Name</label>
+            <input
+              type='text'
+              className='form-control'
+              id='exampleInputEmail1'
+              aria-describedby='emailHelp'
+              placeholder='Enter your first name'
+            />
+          </div>
+          <div className='form-group'>
+            <label htmlFor='InputLastName'>Last Name</label>
+            <input
+              type='email'
+              className='form-control'
+              id='exampleInputEmail1'
+              aria-describedby='emailHelp'
+              placeholder='Enter your last name'
+            />
+          </div>
+          <div className='form-group'>
+            <label htmlFor='InputEmail1'>Email address</label>
+            <input
+              type='email'
+              className='form-control'
+              id='exampleInputEmail1'
+              aria-describedby='emailHelp'
+              placeholder='Enter email'
+            />
+          </div>
+          <div className='form-group'>
+            <label htmlFor='InputPassword'>Password</label>
+            <input
+              type='password'
+              className='form-control'
+              id='exampleInputPassword'
+              placeholder='Password'
+              // onChange={changePassword}
+            />
+          </div>
+          <div className='form-group'>
+            <label htmlFor='InputPassword'>Confirm Password</label>
+            <input
+              type='password'
+              className='form-control'
+              id='exampleInputPassword'
+              placeholder='Confirm Password'
+              // onChange={changePassword}
+            />
+          </div>
+          <button type='submit' className='btn btn-primary'>
+            Create account
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
