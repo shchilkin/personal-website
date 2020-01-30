7238c3ff5ae6bbf3c6f54143c62af48a2225f305
Aleksandr Shchilkin <crazyredkitten@icloud.com>
2020-01-30 22:08:43 +0200

    Updated avatar generator to v. Alpha 0.3

--- src/components/pages/avatarGenerator/AvatarGenerator.js
+++ src/components/pages/avatarGenerator/AvatarGenerator.js
@@ -1,14 +1,11 @@
 import React, { useState } from "react";
-import Avatar from "./Avatar-example.png";
 import AvatarBold from "./Avatars_bold.svg";
 import Hair_1 from "./Hair_1.svg";
 import Hair_2 from "./Hair_2.svg";
 import Hair_3 from "./Hair_3.svg";
 
 const AvatarGenerator = () => {
-  const [hair_1_Visibility, setHair1Visibility] = useState(true);
   const [counter, setCounter] = useState(1);
-  // const [hair_2_Visibility, setHair2Visibility] = useState(false);
   const onClick = () => {
     let Counter = counter;
     if (Counter < 3) {
@@ -18,7 +15,7 @@ const AvatarGenerator = () => {
     }
     setCounter(Counter);
   };
-  console.log("global", counter);
+
   const setHair = counter => {
     if (counter === 1) return Hair_1;
     else if (counter === 2) return Hair_2;
@@ -28,77 +25,50 @@ const AvatarGenerator = () => {
 
   return (
     <div>
-      <h1 className='text-center mb-3'>
-        Avatar Generator <span className='badge badge-info'>v. Alpha 0.1</span>
+      <h1 className='text-center mb-3 mt-3'>
+        Avatar Generator <span className='badge badge-info'>v. Alpha 0.3</span>
       </h1>
-      <img
-        src={AvatarBold}
-        style={{
-          width: "300px",
-          height: "300px",
-          zIndex: 1,
-          position: "absolute"
-        }}
-        class='img-fluid'
-        alt='Responsive'
-      />
-      <img
-        src={setHair(counter)}
-        style={{
-          width: "300px",
-          height: "300px",
-          zIndex: 1,
-          position: "relative"
-        }}
-        class='img-fluid'
-        alt='Responsive'
-      />
-
-      <p className='text-center'>Currently in early stage of development</p>
       <h5 className='text-center'>Avatar:</h5>
-      <div class='d-flex justify-content-center'>
-        <button className='btn btn-danger' onClick={onClick}>
+      <div class='row d-flex justify-content-center'>
+        <img
+          src={AvatarBold}
+          style={{
+            width: "300px",
+            height: "300px",
+            zIndex: 1,
+            position: "absolute"
+          }}
+          className='img-fluid'
+          alt='Responsive'
+        />
+        <img
+          src={setHair(counter)}
+          style={{
+            width: "300px",
+            height: "300px",
+            zIndex: 1,
+            position: "relative"
+          }}
+          className='img-fluid'
+          alt='Responsive'
+        />
+      </div>
+      <p className='text-center'>Currently in early stage of development</p>
+      <p className='text-center'>
+        Only option <span className='badge badge-danger'>Change Hairstyle</span>{" "}
+        works at the moment
+      </p>
+      <div className='container-fluid text-center'>
+        <button className='btn btn-danger mr-2 mb-2' onClick={onClick}>
           Change Hairstyle
         </button>
+        <button className='btn btn-warning mr-2 mb-2'>Change Eye Color</button>
+        <button className='btn btn-secondary mr-2 mb-2'>Change Glasses</button>
+        <button className='btn btn-primary mr-2 mb-2'>Change Skin Color</button>
+        <button className='btn btn-success mr-2 mb-2'>Change Glasses</button>
       </div>
     </div>
   );
 };
 
 export default AvatarGenerator;
-
-// <h5 className='text-center'>Avatar example:</h5>
-// <div class='d-flex justify-content-center'>
-//   <img
-//     src={Avatar}
-//     style={{ width: "300px", height: "300px" }}
-//     class='img-fluid'
-//     alt='Responsive'
-//   />
-// </div>
-
-// {hair_1_Visibility ? (
-//   <img
-//     src={`Hair_${counter}`}
-//     style={{
-//       width: "300px",
-//       height: "300px",
-//       zIndex: 1,
-//       position: "relative"
-//     }}
-//     class='img-fluid'
-//     alt='Responsive'
-//   />
-// ) : (
-//   <img
-//     src={Hair_3}
-//     style={{
-//       width: "300px",
-//       height: "300px",
-//       zIndex: 3,
-//       position: "relative"
-//     }}
-//     class='img-fluid'
-//     alt='Responsive'
-//   />
-// )}
