b418a531c68533387f5adeaa4ade40b7083d51a2
Aleksandr Shchilkin <crazyredkitten@icloud.com>
2020-01-30 21:45:28 +0200

    Avatar generator updated v. Alpha 0.2

--- src/components/pages/avatarGenerator/AvatarGenerator.js
+++ src/components/pages/avatarGenerator/AvatarGenerator.js
@@ -3,14 +3,27 @@ import Avatar from "./Avatar-example.png";
 import AvatarBold from "./Avatars_bold.svg";
 import Hair_1 from "./Hair_1.svg";
 import Hair_2 from "./Hair_2.svg";
+import Hair_3 from "./Hair_3.svg";
 
 const AvatarGenerator = () => {
   const [hair_1_Visibility, setHair1Visibility] = useState(true);
+  const [counter, setCounter] = useState(1);
   // const [hair_2_Visibility, setHair2Visibility] = useState(false);
-
-  const onClick = event => {
-    // event.preventDefault()
-    setHair1Visibility(!hair_1_Visibility);
+  const onClick = () => {
+    let Counter = counter;
+    if (Counter < 3) {
+      Counter += 1;
+    } else {
+      Counter = 1;
+    }
+    setCounter(Counter);
+  };
+  console.log("global", counter);
+  const setHair = counter => {
+    if (counter === 1) return Hair_1;
+    else if (counter === 2) return Hair_2;
+    else if (counter === 3) return Hair_3;
+    return null;
   };
 
   return (
@@ -29,31 +42,17 @@ const AvatarGenerator = () => {
         class='img-fluid'
         alt='Responsive'
       />
-      {hair_1_Visibility ? (
-        <img
-          src={Hair_1}
-          style={{
-            width: "300px",
-            height: "300px",
-            zIndex: 1,
-            position: "relative"
-          }}
-          class='img-fluid'
-          alt='Responsive'
-        />
-      ) : (
-        <img
-          src={Hair_2}
-          style={{
-            width: "300px",
-            height: "300px",
-            zIndex: 3,
-            position: "relative"
-          }}
-          class='img-fluid'
-          alt='Responsive'
-        />
-      )}
+      <img
+        src={setHair(counter)}
+        style={{
+          width: "300px",
+          height: "300px",
+          zIndex: 1,
+          position: "relative"
+        }}
+        class='img-fluid'
+        alt='Responsive'
+      />
 
       <p className='text-center'>Currently in early stage of development</p>
       <h5 className='text-center'>Avatar:</h5>
@@ -77,3 +76,29 @@ export default AvatarGenerator;
 //     alt='Responsive'
 //   />
 // </div>
+
+// {hair_1_Visibility ? (
+//   <img
+//     src={`Hair_${counter}`}
+//     style={{
+//       width: "300px",
+//       height: "300px",
+//       zIndex: 1,
+//       position: "relative"
+//     }}
+//     class='img-fluid'
+//     alt='Responsive'
+//   />
+// ) : (
+//   <img
+//     src={Hair_3}
+//     style={{
+//       width: "300px",
+//       height: "300px",
+//       zIndex: 3,
+//       position: "relative"
+//     }}
+//     class='img-fluid'
+//     alt='Responsive'
+//   />
+// )}
