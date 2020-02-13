075b57b9829543bc21be32ccd9157d5a59eb6842
Aleksandr Shchilkin <crazyredkitten@icloud.com>
2020-02-13 21:59:15 +0200

    Refactor: Little cleanup in server.js

--- server.js
+++ server.js
@@ -11,21 +11,13 @@ app.use(express.static(path.join(__dirname, 'client/build')));
 //  Production mode
 if(process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, 'client/build')));
-    app.get('*', (req, res) => {
-        res.sendfile(path.join(__dirname = 'client/build/index.html'));
-    })
+    app.get('*', (req, res) => res.sendfile(path.join(__dirname = 'client/build/index.html')))
 }
 
 //  Build mode
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname+'/client/public/index.html'));
-});
+app.get('*', (req, res) => res.sendFile(path.join(__dirname+'/client/public/index.html')));
 
 //  Start server
 const port = process.env.PORT || 5000;
-app.listen(port, (req, res) => {  console.log( `server listening on port: ${port}`);});
-
-
-// //  Route setup
-// app.get('/', (req, res) => {    res.send('root route');});
+app.listen(port, () => console.log( `server listening on port: ${port}`));
 
