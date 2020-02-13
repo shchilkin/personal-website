53dd37039204e04f83db6df6445f600720cec374
Aleksandr Shchilkin <crazyredkitten@icloud.com>
2020-02-13 11:19:16 +0200

    Fixed: Fixed card grid display error on small screens (Mobile devices)

--- client/src/components/pages/smallProjects/FindOnGithub/FindOnGithub_Components/Users/Users.component.jsx
+++ client/src/components/pages/smallProjects/FindOnGithub/FindOnGithub_Components/Users/Users.component.jsx
@@ -1,23 +1,18 @@
 import React from "react";
 import User from './UserItem.component';
 import Spinner from "../Spinner.component";
+import './users.style.css'
 
 const Users = ({users, loading}) => {
     if(loading){
         return <Spinner/>
     } else {
-        return (<div style={userStyle}>
+        return (<div className={'usercards'}>
             {users.map(user =>
                 (<User key={user.id} login={user.login} avatar_url={user.avatar_url}  html_url={user.html_url} />)
             )}
         </div>)
     }
-};
-
-const userStyle = {
-    display: 'grid',
-    gridTemplateColumns: 'repeat(5, 1fr)',
-    gridGap: '1rem'
 };
 
 export default Users;
