13a052d203994a2df19fccc696eba34330b6cd7d
Aleksandr Shchilkin <crazyredkitten@icloud.com>
2020-02-20 18:45:49 +0200

    Added: Connected DB to app

---
+++ db.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const db = process.env.MONGO_DB_URI;
+
+const connectDB = async () => {
+  console.log("db", db);
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true
+    });
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.error(error.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,27 +1,33 @@
-const express = require('express');
-const secure = require('express-force-https');
-const path = require('path');
+const express = require("express");
+const secure = require("express-force-https");
+const path = require("path");
+const connectDB = require("./db");
 
 const app = express();
 
+//  Connect MongoDB
+connectDB();
+
 app.use(secure);
 //  Static files
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, "client/build")));
 
 //  Production mode
-if(process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, 'client/build')));
-    app.get('/*', (req, res) => res.sendfile(path.join(__dirname = 'client/build/index.html')))
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client/build")));
+  app.get("/*", (req, res) =>
+    res.sendfile(path.join((__dirname = "client/build/index.html")))
+  );
 }
 
 //  Build mode
-app.get('/*', (req, res) => res.sendFile(path.join(__dirname+'/client/public/index.html')));
+app.get("/*", (req, res) =>
+  res.sendFile(path.join(__dirname + "/client/public/index.html"))
+);
 
-app.use('/api/users', require('./routes/users'));
-app.use('/api/auth', require('./routes/auth'));
-
+app.use("/api/users", require("./routes/users"));
+app.use("/api/auth", require("./routes/auth"));
 
 //  Start server
 const port = process.env.PORT || 5000;
-app.listen(port, () => console.log( `server listening on port: ${port}`));
-
+app.listen(port, () => console.log(`server listening on port: ${port}`));
