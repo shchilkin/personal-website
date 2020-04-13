e78e2ef4fa3f489e779d7605671d0d5c38667b15
Aleksandr Shchilkin <crazyredkitten@icloud.com>
2020-04-13 07:51:42 +0300

    Removed footer & other minor changes
    Name in start page is now a big bigger (96px)
    Added unique brand colors to contactLink buttons

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,5 +1,4 @@
 import React, {Fragment, useContext} from "react";
-import '../components/Layout/Navbar/navbar-item.css';
 import ThemeContext from '../contexts/theme/ThemeContext';
 import {Link} from "react-router-dom";
 import Button from "./Layout/Buttons/Button/Button.component";
