ed01397fee2ab22559caa6bb5d2dc34f7b6febcc
Aleksandr Shchilkin <crazyredkitten@icloud.com>
2020-04-15 11:06:25 +0300

    Now Color setState works synchronously
    small layout changes in soft UI generator

--- client/src/components/pages/NeumorphicGenerator/SoftUIGenerator.js
+++ client/src/components/pages/NeumorphicGenerator/SoftUIGenerator.js
@@ -1,15 +1,8 @@
-import React, {useState, useEffect} from "react";
-import Page from "../../Layout/Page/Page.component";
+import React, {useState} from "react";
 import {Link} from "react-router-dom";
 import Badge from "../../Layout/Badge/Badge.component";
 import Input from "../../Layout/Inputs/Input.component";
 
-
-function incrementFooBy(delta) {
-    return (previousState, currentProps) => {
-        return { ...previousState, foo: previousState.foo + delta };
-    };
-}
 
 function fontColor(red, green, blue) {
     let luminance = ((0.299 * red) + (0.587 * green) + (0.114 * blue))/255;
@@ -21,9 +14,19 @@ function fontColor(red, green, blue) {
     }
 }
 
+function useAsyncState(initialValue) {
+    // https://sung.codes/blog/2018/12/07/setting-react-hooks-states-in-a-sync-like-manner/
+    const [value, setValue] = useState(initialValue);
+    const setter = x =>
+        new Promise(resolve => {
+            setValue(x);
+            resolve(x);
+        });
+    return [value, setter];
+}
 
 const SoftUIGenerator = () => {
-    const [color, setColor] = useState("0xED2939")
+    const [color, setColor] = useAsyncState("0xED2939")
     const [Red, setRed] = useState(237);
     const [Green, setGreen] = useState(41);
     const [Blue, setBlue] = useState(57);
@@ -32,17 +35,15 @@ const SoftUIGenerator = () => {
     const font = fontColor(Red, Green, Blue)
 
     function hexToRGB(color) {
-        console.log('color', color)
         if (color.length === 5) {
-            let _red = parseInt(color[2])
+            setRed(parseInt(`0x${color.slice(2, 3)}${color.slice(2, 3)}`))
+            setGreen(parseInt(`0x${color.slice(3, 4)}${color.slice(3, 4)}`))
+            setBlue(parseInt(`0x${color.slice(4, 5)}${color.slice(4, 5)}`))
         }
         if (color.length === 8){
             setRed(parseInt(`0x${color.slice(2, 4)}`))
             setGreen(parseInt(`0x${color.slice(4, 6)}`))
             setBlue(parseInt(`0x${color.slice(6, 8)}`))
-            console.log('hex', color)
-            console.log('hex parse', parseInt(color))
-            console.log('hex to decimal attempt', parseInt(color,10))
         }
     }
 
@@ -118,76 +119,87 @@ const SoftUIGenerator = () => {
         }
     };
     const onChangeColor = (event) => {
-        let hexColor = event.target.value
-        setColor(incrementFooBy(event.target.value))
-        console.log('x', incrementFooBy(event.target.value))
-
-
-        if (hexColor.indexOf("#") !== -1 ){
-            const hexColorWithoutHash = hexColor.replace(/#/,'')
-            setColor(`0x${hexColorWithoutHash}`)
-            hexToRGB(color)
-        } else {
-            hexToRGB(color)
-            setColor(`0x${event.target.value}`)
-        }
+        setColor(event.target.value).then(value => {
+            console.log('x', value)
+            if (value.indexOf("#") !== -1 ){
+                console.log('in IF', value)
+                const hexColorWithoutHash = value.replace(/#/,'')
+                setColor(`0x${hexColorWithoutHash}`).then(color => hexToRGB(color))
+            } else {
+                console.log('in else', value)
+                setColor(`0x${value}`).then(color => hexToRGB(color))
+            }
+        })
     };
 
+   const containerStyle = {
+       width:'75%',
+       height:'200px',
+       backgroundColor: mainColor,
+       color: font,
+       mixBlendMode: 'normal',
+       boxShadow: `5px 5px ${Blur}px 0 ${darkerShadow},
+       -5px -5px ${Blur}px 0 ${lighterShadow}`,
+       border: `1px solid ${mainColor}`,
+       borderRadius: `12px`
+    }
+
     return(
-        <div className={"pt-5 pb-3"} style={{minHeight:'100vh',backgroundColor:mainColor, color:font}}>
-            <div className={'container'}>
-                <Link to='/' style={{color:'#ed2939'}}>
-                    Back to the homepage
-                </Link>
-                <h3>Soft-UI generator</h3>
-                <h5>Version <Badge type={'small'}>0.2</Badge></h5>
-                <h6>Currently in early stage of development</h6>
-                <div className='row mt-4 mb-4' style={{marginRight:'0px', marginLeft:'0px'}}>
-                    <div className={'col-md-6'}>
-                        <div className={'d-flex justify-content-center'}
-                             style={{width:'100%', height:'300px',
-                                 borderRadius:'12px',
-                                 backgroundColor:mainColor}}>
-                            <div
-                                className={'align-self-center'}
-                                style={{
-                                    width:'75%',
-                                    height:'200px',
-                                    backgroundColor:mainColor,
-                                    borderRadius:'12px',
-                                    boxShadow: `10px 10px ${Blur}px 0
-                                    ${darkerShadow},
-                                    -10px -10px ${Blur}px 0
-                                    ${lighterShadow}`
-                            }}/>
+        <div className={"page"}>
+            <div className={'container-fluid pb-5'} style={{backgroundColor:mainColor, color:font}}>
+                <div className={'container'}>
+                    <Link to='/' style={{color:'#ed2939'}}>
+                        Back to the homepage
+                    </Link>
+                    <h3>Soft-UI generator</h3>
+                    <h5>Version <Badge type={'small'}>0.3</Badge></h5>
+                    <h6>Currently in early stage of development</h6>
+                    <div className='row mt-4 mb-4' style={{marginRight:'0px', marginLeft:'0px'}}>
+                        <div className={'col-md-6'}>
+                            <div className={'d-flex justify-content-center'}
+                                 style={{width:'100%', height:'300px',
+                                     borderRadius:'12px',
+                                     backgroundColor:mainColor}}>
+                                <div
+                                    className={'align-self-center'}
+                                    style={containerStyle}>Soft UI container</div>
+                            </div>
                         </div>
-                    </div>
-                    <div className={'col-md-6 mt-3'}>
-                        {/*<h6>Enter hexadecimal color code</h6>*/}
-                        {/*<Input type={'text'}  onChange={onChangeColor} value={color.replace(/0x/,'')} placeholder={'#'}/>*/}
-                        <h6 style={{color:'#ed2939', fontWeight:'bold'}}>Red</h6>
-                        <Input type={'number'} onChange={onChangeRed} value={Red} placeholder={Red}/>
-                        <h6 style={{color:'#0B6623', fontWeight:'bold'}}>Green</h6>
-                        <Input type={'number'}  onChange={onChangeGreen} value={Green} placeholder={Green}/>
-                        <h6 style={{color:'#0f52Ba', fontWeight:'bold'}} >Blue</h6>
-                        <Input type={'number'} onChange={onChangeBlue} value={Blue} placeholder={Blue}/>
-                        <h6>Blur</h6>
-                        <Input type={'number'} onChange={onChangeBlur} value={Blur} placeholder={Blur}/>
-                        <div>
-                            Light shadow:{" "}
-                            <Badge>{lighterShadows[0]}</Badge>
-                            <Badge color={'Green'}>{lighterShadows[1]}</Badge>
-                            <Badge color={'Blue'}>{lighterShadows[2]}</Badge>
-                        </div>
-                        <div>
-                            Dark shadow:{" "}
-                            <Badge>{darkerShadows[0]}</Badge>
-                            <Badge color={'Green'}>{darkerShadows[1]}</Badge>
-                            <Badge color={'Blue'}>{darkerShadows[2]}</Badge>
-                        </div>
-                        <div>
-                            Other:{" "}
-                            <Badge>Blur: {Blur}px</Badge>
+                        <div className={'col-md-6 mt-3'}>
+                            <h6>Enter hexadecimal color code</h6>
+                            <Input type={'text'}  onChange={onChangeColor} value={color.replace(/0x/,'#')} placeholder={'#'}/>
+                            <div className={'row'}>
+                                <div className={'col-md-4'}>
+                                    <h6 style={{color:'#ed2939', fontWeight:'bold'}}>Red</h6>
+                                    <Input type={'number'} onChange={onChangeRed} value={Red} placeholder={Red}/>
+                                </div>
+                                <div className={'col-md-4'}>
+                                    <h6 style={{color:'#0B6623', fontWeight:'bold'}}>Green</h6>
+                                    <Input type={'number'}  onChange={onChangeGreen} value={Green} placeholder={Green}/>
+                                </div>
+                                <div className={'col-md-4'}>
+                                    <h6 style={{color:'#0f52Ba', fontWeight:'bold'}} >Blue</h6>
+                                    <Input type={'number'} onChange={onChangeBlue} value={Blue} placeholder={Blue}/>
+                                </div>
+                            </div>
+                            <h6>Blur</h6>
+                            <Input type={'number'} onChange={onChangeBlur} value={Blur} placeholder={Blur}/>
+                            <div>
+                                Light shadow:{" "}
+                                <Badge>{lighterShadows[0]}</Badge>
+                                <Badge color={'Green'}>{lighterShadows[1]}</Badge>
+                                <Badge color={'Blue'}>{lighterShadows[2]}</Badge>
+                            </div>
+                            <div>
+                                Dark shadow:{" "}
+                                <Badge>{darkerShadows[0]}</Badge>
+                                <Badge color={'Green'}>{darkerShadows[1]}</Badge>
+                                <Badge color={'Blue'}>{darkerShadows[2]}</Badge>
+                            </div>
+                            <div>
+                                Other:{" "}
+                                <Badge>Blur: {Blur}px</Badge>
+                            </div>
                         </div>
                     </div>
                 </div>
